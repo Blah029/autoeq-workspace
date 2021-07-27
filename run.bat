9c74568cca9be7bb152468c71f5ec7e7563f3111
Blah029 <81973454+Blah029@users.noreply.github.com>
2021-07-27 15:48:34 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-set "MODEL=<model>"
-set "COMPENSATION=<compensation method>"
-venv\Scripts\activate.bat && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%\%MODEL% %COMPENSATION%" --compensation="compensation/%COMPENSATION%.csv" --equalize --parametric_eq --max_filters=5+5 --ten_band_eq --convolution_eq --bass_boost=6 --standardize_input
+set "MODEL=BeoPlay A1"
+set "COMPENSATION=zero"
+venv\Scripts\activate.bat && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%" --compensation="compensation/%COMPENSATION%.csv" --equalize --parametric_eq --max_filters=5+5 --ten_band_eq --convolution_eq --bass_boost=6 --standardize_input
