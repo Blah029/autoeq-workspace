132f3eb0ffd2e315b2c4c58ee2b92ff5375d9b5f
Blah029 <81973454+Blah029@users.noreply.github.com>
2022-06-04 12:41:14 +0530

    Add files via upload

--- autoeq_commands.bat
+++ autoeq_commands.bat
@@ -1,5 +1,6 @@
-set "MODEL=BeoPlay A1"
-set "COMPENSATION=zero"
+set "MODEL="
+set "COMPENSATION="
 set "BASSBOOST=0"
+set "SAMPLERATE=48000"
 
-venv\Scripts\activate.bat && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%" --compensation="compensation/%COMPENSATION%.csv" --equalize --ten_band_eq --convolution_eq --fs=44100 --bass_boost=%BASSBOOST% --standardize_input && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%\7BandEQ" --compensation="compensation/%COMPENSATION%.csv" --equalize  --fixed_band_eq --fc=60,150,400,1000,3000,8000,16000 --q=1.12,1.04,1.12,0.92,1.04,1.41,1.41 --bass_boost=%BASSBOOST% --standardize_input && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%\9BandEQ" --compensation="compensation/%COMPENSATION%.csv" --equalize  --fixed_band_eq --fc=63,125,250,500,1000,2000,4000,8000,16000 --q=1.41 --bass_boost=%BASSBOOST% --standardize_input
+venv\Scripts\activate.bat && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%" --compensation="compensation/%COMPENSATION%.csv" --equalize --ten_band_eq --convolution_eq --fs=%SAMPLERATE% --bass_boost=%BASSBOOST% --standardize_input && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%\7BandEQ" --compensation="compensation/%COMPENSATION%.csv" --equalize  --fixed_band_eq --fc=60,150,400,1000,3000,8000,16000 --q=1.12,1.04,1.12,0.92,1.04,1.41,1.41 --bass_boost=%BASSBOOST% --standardize_input && python autoeq.py --input_dir="my_data\%MODEL%" --output_dir="my_results\%MODEL%\9BandEQ" --compensation="compensation/%COMPENSATION%.csv" --equalize  --fixed_band_eq --fc=63,125,250,500,1000,2000,4000,8000,16000 --q=1.41 --bass_boost=%BASSBOOST% --standardize_input
